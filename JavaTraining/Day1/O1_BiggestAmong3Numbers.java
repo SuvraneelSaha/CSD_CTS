5f1a13fb084562cd670cd6c651f36da47f88b547
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-12-27 04:27:11 +0000

    Biggest Number among 3 code

---
+++ JavaTraining/Day1/O1_BiggestAmong3Numbers.java
@@ -0,0 +1,22 @@
+package JavaTraining.Day1;
+
+import java.util.Scanner;
+
+public class O1_BiggestAmong3Numbers {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Please enter the 3 Numbers : ");
+
+    int a = in.nextInt();
+
+    int b = in.nextInt();
+
+    int c = in.nextInt();
+
+    System.out.println("Maximum among the 3 numbers is "+Math.max(Math.max(a, b),c));
+
+
+    
+  }
+}
