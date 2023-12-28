ce7d7bc41d2a238e6b2f451d241939cd9d5c352c
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-12-28 05:11:31 +0000

    java strings part 2

---
+++ JavaTraining/Day2/JavaStrings/O2.java
@@ -0,0 +1,12 @@
+package JavaTraining.Day2.JavaStrings;
+
+public class O2 {
+  public static void main(String[] args) {
+    // replace single or multiple characters in a string 
+
+    String s1 = "Welcome to java Full Stack Development" ; 
+
+    
+  }
+  
+}
