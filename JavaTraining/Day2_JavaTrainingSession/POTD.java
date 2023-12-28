053afe76dfd1c4dd8af39dc7eb5605ea73dfb8b3
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-12-28 12:15:40 +0000

    potd

---
+++ JavaTraining/Day2_JavaTrainingSession/POTD.java
@@ -0,0 +1,48 @@
+package JavaTraining.Day2_JavaTrainingSession;
+
+import java.util.Scanner;
+// Palindromic string 
+public class POTD {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.println("Enter the String:");
+
+    String str = in.next();
+
+   System.out.println(str);
+
+   if(isPalindromeString(str) == true)
+   {
+    System.out.println("Palindromic String");
+   }
+   else
+   {
+    System.out.println("NA");
+   }    
+   
+
+  }
+  public static boolean isPalindromeString(String str)
+  {
+    String rev ="";
+
+    for(int i = str.length()-1; i>=0;i--)
+    {
+      char ch = str.charAt(i) ;
+      
+      rev = rev + ch;
+
+    }
+
+    if(rev.equals(str))
+    {
+
+      return true ; 
+    }
+    else 
+    {
+      return false ;
+    }
+  }
+}
---
+++ JavaTraining/Day2_JavaTrainingSession/POTDA.java
@@ -0,0 +1,26 @@
+package JavaTraining.Day2_JavaTrainingSession;
+
+import java.util.Scanner;
+
+public class POTDA {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter a String :");
+
+    String str = in.nextLine();
+
+    StringBuffer str1 = new StringBuffer();
+
+    str1.append(str) ; 
+
+    str1.reverse();
+    // physically reverse of the string 
+
+    System.out.println(str1);
+
+    String str2 = str1.toString() ; 
+
+    System.out.println(str2.equals(str) ? "Palindrome" : "NA");
+  }
+}
