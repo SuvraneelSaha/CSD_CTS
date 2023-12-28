e7b1f13f726187d36b30bbe67603ee0ae56a1f05
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-12-28 23:42:57 +0530

    substring strings

---
+++ JavaTraining/JavaStringsUdemy/O3.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class O3 {
+
+  public static void main(String[] args) {
+    
+    String s = "Welcome to java FSE Development";
+
+    // replace() function 
+
+    System.out.println(s.replace('e','X'));
+
+    //the actual original string does not gets changed 
+    // a new string is created 
+
+    String snew = s.replace('e', 'X') ; 
+
+    System.out.println(s);
+
+    System.out.println(snew);
+
+    // the replace() func makes a new string out of the string that is used 
+
+
+    // replace sequence of characters ie sequentially 
+
+    System.out.println(s.replace("java", "JS"));
+
+
+    // substring 
+
+    // use this method substring from the main string 
+
+
+    /*
+      str = "Welcome";
+
+      pass starting and ending index numbers 
+
+      substring(s,e) ; 
+
+      Welcome
+      0123456
+
+      we want from c to e 
+
+      substring(3,7) ;
+      // ending index - 7 
+      // come 
+
+      substring(2,6)
+      // in between 
+      // lcom 
+
+
+      substring(0,3)
+      // wel
+
+
+
+
+
+     */
+
+
+     String str = "Welcome";
+
+     System.out.println(str.substring(2,5));
+
+
+
+  }
+}
