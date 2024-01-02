b28140fe9e806c809725cacd4540244c2d32f132
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-02 04:28:16 +0000

    POTD

---
+++ JavaTraining/Day4/O1.java
@@ -0,0 +1,15 @@
+package JavaTraining.Day4;
+
+import java.util.Scanner;
+
+public class O1 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+
+  }
+}
+
+/*
+ 
+ */
