1748047b381b403cb30a39db98ad131a0e27d896
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-08 04:36:20 +0000

    Week 3 Stringbuffer topic

---
+++ JavaTraining/Week3/O2.java
@@ -0,0 +1,23 @@
+package JavaTraining.Week3;
+
+public class O2 {
+  public static void main(String[] args) {
+    StringBuffer sb = new StringBuffer("Welcome");
+    
+    sb.append(" To Java");
+
+    // stringbuffer vs stringbuilder 
+    // in case of stringbuffer it is thread safe 
+    // and in case of stringbuilder it is not thread safe 
+
+    String str = new String("Welcome");
+    str.concat(" To Java");
+
+    // stringbuffer does not allows multi threaded actions 
+
+    // in case of stringbuilder allows more than one thread 
+
+    
+
+  }
+}
