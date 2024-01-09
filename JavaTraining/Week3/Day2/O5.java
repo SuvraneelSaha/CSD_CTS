fc37c3bcd2e387e7b665d2dae2342517e6bbd616
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-09 05:57:55 +0000

    week 3 day 2

---
+++ JavaTraining/Week3/Day2/O5.java
@@ -0,0 +1,47 @@
+package JavaTraining.Week3.Day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import java.util.*;
+
+public class O5 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("enter the number of elements in the array :");
+
+    int n = in.nextInt();
+    int[] arr = new int[n];
+
+
+    System.out.println("enter the elements : ");
+
+
+    for(int i = 0 ; i < arr.length;i++)
+    {
+      arr[i] = in.nextInt();
+    }
+
+
+
+  }
+
+  public static int[] removeElement(int[] arr)
+  {
+   
+    List<Integer> list = Collections.addAll(arr);
+
+
+
+    
+
+    for (int i = 0; i < arr.length; i++) {
+      
+    }
+
+    
+
+  }
+}
