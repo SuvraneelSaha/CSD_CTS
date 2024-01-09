ff262bc17b3e391ae8a434297f28f2b9982c4cc0
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-09 05:13:57 +0000

    Inheritance question function left

---
+++ JavaTraining/Week3/Day2/Main.java
@@ -0,0 +1,83 @@
+// package JavaTraining.Week3.Day2;
+
+// public class Main {
+//   public static void main(String[] args) {
+    
+//   }
+// }
+
+// public class Product{
+//   private int pid;
+//   private String pname ; 
+//   private float price ; 
+
+//   public Product(int pid,String pname,float price)
+//   {
+//     super();
+//     this.pid = pid;
+//     this.pname = pname;
+//     this.price = price;
+
+//   }
+
+//   public int getPid()
+//   {
+//     return pid;
+//   }
+
+//   public void setPid(int pid)
+//   {
+//     this.pid = pid;
+//   }
+
+//   public String getPname()
+//   {
+//     return pname;
+//   }
+
+//   public void setPname(String pname)
+//   {
+//     this.pname = pname;
+//   }
+
+//   public float getPrice()
+//   {
+//     return price;
+//   }
+
+//   public void setPrice(float price)
+//   {
+//     this.price = price ; 
+//   }
+
+//  class abstract void discountCalculation();
+// }
+
+//  class Clothes extends Product{
+ 
+// 	public Clothes(int pid, String pname, float price) {
+// 		super(pid, pname, price);
+// 		// TODO Auto-generated constructor stub
+// 	}
+ 
+// 	@Override
+// 	public void discountCalculation() {
+// 		// TODO Auto-generated method stub
+// 	}
+ 
+// }
+
+//  class Accessories extends Product{
+ 
+// 	public Accessories(int pid, String pname, float price) {
+// 		super(pid, pname, price);
+// 		// TODO Auto-generated constructor stub
+// 	}
+
+ 
+// 	@Override
+// 	public void discountCalculation() {
+// 		// TODO Auto-generated method stub
+// 	}
+ 
+// }
---
+++ JavaTraining/Week3/Day2/O1.java
@@ -0,0 +1,36 @@
+package JavaTraining.Week3.Day2;
+/*
+	 * I need one application for Inventory Management. I have two different 
+	 * categories of products (Accessories and clothes) . Discount Caluclation 
+	 * is differntb for each category.
+	 * 
+	 * for Accessories 10% discount from total amount
+	 * and for clothes 30% discount from each product price if the discount 
+	 * exceeds 100Rs then set discount as 120Rs. 
+	 */
+public class O1 {
+  public static void main(String[] args) {
+    
+  }
+}
+class Accessories{
+  private int price;
+  private double discounted_Price;
+
+
+  public void setAcc_Price(int price) {
+    this.price = price;
+  }
+
+   public double getDiscountedPrice() {
+    return 0.0;
+  }
+
+
+
+}
+
+class Clothes{
+private double width;
+  private double height;
+}
