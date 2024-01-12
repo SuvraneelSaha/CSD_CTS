a32d11a438fe4d39afddc7b3ccde0cc4491f24a2
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-12 13:21:17 +0000

    index of an extra element question

---
+++ JavaTraining/Week3/Day5/POTD.java
@@ -0,0 +1,19 @@
+package JavaTraining.Week3.Day5;
+
+import java.util.Scanner;
+
+public class POTD {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the number of  elements in the array : ");
+    int n = in.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; i++) {
+      a[i] = in.nextInt();
+    }
+
+    
+
+
+  }
+}
