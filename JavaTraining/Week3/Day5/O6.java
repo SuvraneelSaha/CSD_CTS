ffab8ea1185cb1e94cb3e2136ef02dd679dcbddf
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-12 05:23:36 +0000

    string module

---
+++ JavaTraining/Week3/Day5/O6.java
@@ -0,0 +1,11 @@
+package JavaTraining.Week3.Day5;
+
+import java.util.Scanner;
+
+public class O6 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    
+  }
+}
