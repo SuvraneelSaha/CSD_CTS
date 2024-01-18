df91a23a7ac0754c99714185b0b99698d44539b7
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-01-18 14:05:47 +0000

    Least Offer Question Done

--- JavaTraining/TechStac/Day2/O5.java
+++ JavaTraining/TechStac/Day2/O5.java
@@ -1,44 +1,72 @@
-import java.util.*;
-import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class O5 {
   public static void main(String[] args) {
+
     Scanner in = new Scanner(System.in);
-    
+
     int n = Integer.parseInt(in.nextLine());
 
-    TreeMap<Integer,String> map = new TreeMap<>();
+    String[] pName = new String[n];
+
+    int[] pPrice = new int[n];
+
+    int[] pRate = new int[n];
+
+    int[] pDiscount = new int[n];
+
+
 
     for (int i = 0; i < n; i++)
     {
-        String input = in.nextLine();
-        String[] str=input.split("[,]+");  
-        System.out.println(Arrays.toString(str));
-        System.out.println(str[0]);
+      String input = in.nextLine();
 
-        String keyName = str[0];
-        int price = Integer.parseInt(str[1]);
-        int rate = Integer.parseInt(str[2]);
+      String[] str=input.split("[,]+"); 
 
-        int discount = (price * rate)/100;
+       System.out.println(Arrays.toString(str));
 
-        map.put(discount,keyName);
+       System.out.println(str[0]);
 
+       String keyName = str[0];
+
+       int price = Integer.parseInt(str[1]);
+
+       int rate = Integer.parseInt(str[2]);
+
+       int discount = (price * rate)/100;
+
+       pName[i] = keyName;
+       pPrice[i] = price;
+       pRate[i] = rate;
+       pDiscount[i] = discount;
     }
 
-    int lowDiscount = map.firstKey();
+    System.out.println("Product Name"+Arrays.toString(pName));
+    System.out.println("Product Price"+Arrays.toString(pPrice));
+    System.out.println("Product Discount Rate"+Arrays.toString(pRate));
+    System.out.println("Product Discount"+Arrays.toString(pDiscount));
 
-    System.out.println(lowDiscount);
-   
-   for(Map.Entry<Integer,String> e : map.entrySet())
-   {
-      if(lowDiscount==e.getKey())
+    int low = pDiscount[0] ; 
+    int lowIndex;
+    for (int i = 1; i < pDiscount.length; i++)
+    {
+      if(pDiscount[i] < low)
       {
-        System.out.println(e.getValue());
+        low = pDiscount[i];
+        lowIndex = i;
       }
-   }
-   
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      if(low==pDiscount[i])
+      {
+        System.out.println(pName[i]);
+      }  
+    }
 
 
-    
+
   }
 }
